114d608e3285dc6bdfe97e519dd19934404fe73f
Mathieu Adam <mathieu.adam@ingenico.com>
2020-05-12 22:35:36 +0200

    Add characters json cost, add new pages

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,28 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'moussaillon'}
+    lists = [
+        {
+            'author': {'username': 'Garag'},
+            'name': 'La bande à Dudule',
+            'faction': 'Sundars',
+            'format': '1000 PO'
+        },
+        {
+            'author': {'username': 'Manu'},
+            'name': 'Le Cartel de Nachos de la Riviera',
+            'faction': 'Quintors',
+            'format': '400 PO'
+        }
+    ]
+    return render_template('index.html', title='Briskars', user=user, lists=lists)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
