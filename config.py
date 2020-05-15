80f1e0e3e79dcebb576bdb10c33cebc596ca2fb3
Mathieu Adam <mathieu.adam@ingenico.com>
2020-05-15 13:38:59 +0200

    Add maoks profile

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ta-gueule-c-est-magique'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite3:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
